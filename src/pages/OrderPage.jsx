a75ae7600d7c8cc5516b327be9ed634930780ae9
Edgar Garcia <edgargarciafcb@gmail.com>
2024-10-30 18:47:15 -0600

    Solucionando errores de nombres de css

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './OrderPage.css';
+import './orderPage.css';
 
 const OrderPage = () => {
     const { id_usuario, id_pedido } = useParams();
